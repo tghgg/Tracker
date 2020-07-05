715436b3b1880c342d290baec80b73dd407211b6
Falling Snowdin <falling_snowdin@protonmail.com>
2020-07-06 02:37:26 +0700

    Kill the remote font; make creating multiple tasks easier

--- src/subwindows/input_window.js
+++ src/subwindows/input_window.js
@@ -9,6 +9,10 @@ window.onblur = () => {
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
   ipcRenderer.send('create-task', document.querySelector('input').value);
+
+  // Reset the input field and focus the input field again
+  document.getElementById('task-name').value = '';
+  document.getElementById('task-name').focus();
 });
 
 document.querySelector('.main > button').addEventListener('click', (event) => {
